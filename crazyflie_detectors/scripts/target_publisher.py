24a9185541154f144da2edea1860e57f7f88f8d8
strakam <strakammm@gmail.com>
2023-10-31 15:14:05 +0100

    create random landing location on the floor

--- crazyflie_detectors/scripts/target_publisher.py
+++ crazyflie_detectors/scripts/target_publisher.py
@@ -12,13 +12,17 @@ class TargetPublisher(Node):
         self.publisher = self.create_publisher(TargetTransformationVector, 'crazyflie/target_transformation_vector', 10)
         timer_period = 1.0 # seconds
         self.timer = self.create_timer(timer_period, self.publish_target)
+
+        self.x_target = random.uniform(-5, 5)
+        self.y_target = random.uniform(-5, 5)
+        self.z_target = 0
     
     def publish_target(self):
         msg = TargetTransformationVector()
-        # random float64
-        msg.x = random.random()
-        msg.y = random.random()
-        msg.z = random.random()
+        # random float64 from range -5 to 5
+        msg.x = self.x_target
+        msg.y = self.y_target
+        msg.z = self.z_target
         self.publisher.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg)
 
