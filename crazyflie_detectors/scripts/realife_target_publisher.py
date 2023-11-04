a3b8cb3ce9f67edc76aee5721042d037a91dd2a9
strakam <strakammm@gmail.com>
2023-11-04 20:34:51 +0100

    camera detector publisher 1.0

--- crazyflie_detectors/scripts/realife_target_publisher.py
+++ crazyflie_detectors/scripts/realife_target_publisher.py
@@ -3,11 +3,8 @@ from typing import List
 import rclpy
 from rclpy.context import Context
 from rclpy.node import Node
-from crazyflie_msgs.msg import TargetTransformationVector
-from geometry_msgs.msg import PointStamped
-from rclpy.parameter import Parameter
+from crazyflie_msgs.msg import TargetTransformation
 from sensor_msgs.msg import Image
-import random
 import dt_apriltags as apriltag
 import cv2
 import numpy as np
@@ -17,7 +14,7 @@ from cv_bridge import CvBridge
 class ApriltagDetector(Node):
     def __init__(self):
         super().__init__('crazyflie_detectors')
-        self.publisher = self.create_publisher(TargetTransformationVector, 'crazyflie/target_transformation_vector', 10)
+        self.publisher = self.create_publisher(TargetTransformation, 'crazyflie/target_transformation', 10)
         self.camera_sub = self.create_subscription(Image, 'esp_32/camera', self.camera_callback, 10)
         self.publish_timer = self.create_timer(0.01, self.publish_target)
 
@@ -38,9 +35,8 @@ class ApriltagDetector(Node):
             debug=0
         )
 
-        self.target_x = 0.0
-        self.target_y = 0.0
-        self.target_z = 0.0
+        self.translation = np.zeros(3)
+        self.rotation = np.eye(3)
 
         self.bridge = CvBridge()
     
@@ -60,18 +56,23 @@ class ApriltagDetector(Node):
         )
         if len(detections) > 0:
             tag_pos = detections[0].pose_t
+            rotation = detections[0].pose_R
         else:
             tag_pos = [[0.0], [0.0], [0.0]]
+            rotation = np.eye(3)
 
-        self.target_x = tag_pos[0][0]
-        self.target_y = tag_pos[1][0]
-        self.target_z = tag_pos[2][0]
+        self.translation = np.array([
+            tag_pos[0][0],
+            tag_pos[1][0],
+            tag_pos[2][0]
+        ])
+        self.rotation = rotation
+
 
     def publish_target(self):
-        msg = TargetTransformationVector()
-        msg.x = self.target_x
-        msg.y = self.target_y
-        msg.z = self.target_z
+        msg = TargetTransformation()
+        # concat translation and flattened rotation
+        msg.t = self.translation.flatten().tolist() + self.rotation.flatten().tolist()
         self.publisher.publish(msg)
         self.get_logger().info('Publishing here: "%s"' % msg)
     
