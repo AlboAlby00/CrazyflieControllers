70abfa2c66ab841e44ba66b5d2f0f86f9ffdbb3f
Daniel He <he.daniel@proton.me>
2023-12-05 11:07:47 +0100

    chore: Safety commit for code where the assumption that the initial body frame of the drone is the world frame is not true and we have to use a previous Body frame and a current Body frame of the drone.

--- crazyflie_controllers/include/crazyflie_controllers/position_pid_controller.h
+++ crazyflie_controllers/include/crazyflie_controllers/position_pid_controller.h
@@ -9,6 +9,7 @@
 #include "sensor_msgs/msg/imu.hpp"
 #include <tf2/transform_datatypes.h>
 #include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Transform.h>
 
 #include "crazyflie_controllers/control_utils/pid.h"
 
@@ -36,7 +37,30 @@ private:
     double _roll;
     double _pitch;
     double _yaw;
-    tf2::Matrix3x3 _body_orientation;
+
+    //Rotation quantities of coordinate frames
+    tf2::Matrix3x3 R_WBnow; // Rotation going from world frame W to the current body frame B, cannot name it _R_WBnow
+    // R_WBnow = R_WBold * R_BoldBnow
+
+    // R_BoldBnow is essentially the Rotation we get from the IMU
+    tf2::Matrix3x3 R_BoldBnow; // Rotation going from old body frame to the body frame B now, cannot use prime character
+    // R_WBold is the old (the initial Rotation when it starts) Rotation going from world frame to the old body frame B
+    tf2::Matrix3x3 R_WBold; // Rotation going from world frame W to the current body frame B, cannot name it _R_WB
+
+    //translation quantities of coordinate frames
+    tf2::Vector3 _p_WBnow; // Vector going from the world frame W to the body frame B, essentially holding _x, _y, _z
+
+    //Transformation between coordinate frames
+    tf2::Transform X_WBnow; // Transformation from the world frame to the current body frame of the drone
+    tf2::Transform X_BnowW; // Transformation from the current body frame of the drone to the world frame
+
+    // vector quantities about the desired position vector
+    tf2::Vector3 _p_BnowD; // Vector (measured from body frame B) going from the frame body B to a desired position D
+    tf2::Vector3 _p_WD_Bnow; // Vector measured from world frame going from frame B to a desired position D
+
+    // The goal here is to express the desired position measured in the world frame _p_WD_Bnow in the body frame _p_BnowD,
+    // so  that we use this vector for the position commands of the drone
+    // _p_BnowD = X_BnowW * _p_WD_Bnow
 
     double _input_yaw;
     double _target_x;
--- crazyflie_controllers/src/position_pid_controller.cpp
+++ crazyflie_controllers/src/position_pid_controller.cpp
@@ -2,7 +2,7 @@
 #include "crazyflie_controllers/position_pid_controller.h"
 
 PositionPID::PositionPID() : 
-    Node("attitude_pid_controller"), _input_yaw(0.0), _target_x(0.0), _target_y(0.0), _target_z(0.0),
+    Node("position_pid_controller"), _input_yaw(0.0), _target_x(0.0), _target_y(0.0), _target_z(0.0),
         _pid_x(0.01, 0, 0.04, true, 0.1), _pid_y(0.01, 0, 0.04, true, 0.1), _pid_z(8, 1 , 10, true, 0.1)
 {
         _sub_new_position = this->create_subscription<crazyflie_msgs::msg::PositionCommand>(
@@ -28,24 +28,51 @@ PositionPID::PositionPID() :
             std::bind(&PositionPID::_sendCommandAttitude, this));
 
         _old_time = now();
-  
+
+        R_WBnow.setIdentity();
+
+        R_BoldBnow.setIdentity();
+        R_WBold.setIdentity();
+        X_WBnow.setIdentity();
+
 }
 
 void PositionPID::_newPositionCommandCallback(const crazyflie_msgs::msg::PositionCommand::SharedPtr command)
 {
-    _target_x = command->x;
-    _target_y = command->y;
-    _target_z = command->z;
+    //Assumption Vector measured from world frame going from frame B to a desired position D
+    _p_WD_Bnow = tf2::Vector3(command->x, command->y, command->z);
+
+    // X_BnowW is the Transformation from the current body frame of the drone to the world frame
+    X_BnowW = X_WBnow.inverse();
+
+    // The goal here is to express the desired position measured in the world frame _p_WD_Bnow in the body frame _p_BnowD,
+    // so  that we use this vector for the position commands of the drone
+    _p_BnowD = X_BnowW * _p_WD_Bnow;
+
+    // Step 4: Set the target positions in the drone's local coordinate system.
+    _target_x = _p_BnowD.x();
+    _target_y = _p_BnowD.y();
+    _target_z = _p_BnowD.z();
     _yaw = command->yaw;
-    RCLCPP_INFO(this->get_logger(), "target updated, x: %f, y: %f, z= %f", _target_x, _target_y, _target_z);
+    RCLCPP_INFO(this->get_logger(),
+                "target updated, _target_x = _p_BnowD.x: %f, _target_y = _p_BnowD.y: %f, _target_z = _p_BnowD.z = %f",
+                _target_x, _target_y, _target_z);
 }
 
 void PositionPID::_newGpsCallback(const geometry_msgs::msg::PointStamped::SharedPtr gps_data)
 {
-    _x = gps_data->point.x;
-    _y = gps_data->point.y;
-    _z = gps_data->point.z;
+    // Vector going from the world frame W to the body frame B, essentially holding _x, _y, _z
+    _p_WBnow = tf2::Vector3(gps_data->point.x, gps_data->point.y, gps_data->point.z);
 
+    _x = _p_WBnow.x();
+    _y = _p_WBnow.y();
+    _z = _p_WBnow.z();
+
+    X_WBnow.setOrigin(_p_WBnow);
+
+
+    RCLCPP_INFO(this->get_logger(),
+                 "GPS updated, _x = _p_WBnow.x: %f, _y = _p_WBnow.y(): %f, _z = _p_WBnow.z = %f", _x, _y, _z);
 }
 
 void PositionPID::_newImuCallback(const sensor_msgs::msg::Imu::SharedPtr imu_data) {
@@ -55,39 +82,56 @@ void PositionPID::_newImuCallback(const sensor_msgs::msg::Imu::SharedPtr imu_dat
             imu_data->orientation.z,
             imu_data->orientation.w);
 
-    tf2::Matrix3x3 mat(quaternion);
-    mat.getRPY(_roll, _pitch, _yaw);
-    _body_orientation = mat;
+    // Update the previous Rotation R_WBold to be the R_WBnow from last time
+    R_WBold = R_WBnow;
 
-    RCLCPP_DEBUG(this->get_logger(), "imu data received! [r: %f, p: %f, y: %f]", _roll, _pitch, _yaw);
+    // Rotation going from old body frame to the current body frame Bnow. Informatiion gained from IMU
+    R_BoldBnow = tf2::Matrix3x3(quaternion); // actually a member, but cannot name it _R_WB
+
+    // Combine the previous Rotation with the new Rotation gained from the IMU
+    R_WBnow = R_WBold * R_BoldBnow;
+
+    X_WBnow.setBasis(R_WBnow);
+
+    R_WBnow.getRPY(_roll, _pitch, _yaw);
+
+    RCLCPP_INFO(this->get_logger(),
+                 "imu data received! [r: %f, p: %f, y: %f]",
+                 _roll, _pitch, _yaw);
 }
 
 void PositionPID::_sendCommandAttitude()
 {
     rclcpp::Duration dt = now() - _old_time; 
     
-    double pid_x = _pid_x.getCommand(_x, _target_x, dt);
-    double pid_y = _pid_y.getCommand(_y, _target_y, dt);
-    double pid_z = _pid_z.getCommand(_z, _target_z, dt);
+    double pid_x = _pid_x.getCommand(_p_WBnow.x(), _p_BnowD.x(), dt);
+    double pid_y = _pid_y.getCommand(_p_WBnow.y(), _p_BnowD.y(), dt);
+    double pid_z = _pid_z.getCommand(_p_WBnow.z(), _p_BnowD.z(), dt);
 
 
+    /* Made obsolete with my MIT notation
     // first map a difference in x-, y-, z-direction direction to corresponding gains in attitude gains (and thrust)
     tf2::Vector3 desired_action(pid_x, - pid_y, pid_z); // For the explanation of the - pid_y consult the Readme
 
     // map the desired action to body frame of drone by using the inverse (the transpose for rotation matrices) of the
     // body frame rotation matrix
-    tf2::Vector3 mapped_action = _body_orientation * desired_action;
+    tf2::Vector3 mapped_action = R_WB * desired_action;
+     */
 
     auto msg = std::make_unique<crazyflie_msgs::msg::AttitudeCommand>();
 
-    msg->pitch = mapped_action.x();
-    msg->roll = mapped_action.y();
-    msg->thurst = mapped_action.z();
+    msg->pitch = pid_x;
+    msg->roll = -pid_y;
+    msg->thurst = pid_z;
     msg->yaw = _yaw;
 
     _pub_attutude_cmd->publish(std::move(msg));
 
     _old_time = now();
+
+    RCLCPP_INFO(this->get_logger(),
+                 "Sent out Commands! [pitch = pid_x: %f, roll = -pid_y: %f, thurst = pid_z: %f]",
+                 pid_x, -pid_y, pid_z);
 }
 
 int main(int argc, char const *argv[])
